ffdd4de8943952c095124d459637516d1f8fd2cb
Samuel Nunoo <38366722+samuelnunoo@users.noreply.github.com>
2021-05-08 11:27:39 -0700

    removed build files from git

--- nuxt/.nuxt-storybook/components/index.js
+++
@@ -1,9 +0,0 @@
-import { wrapFunctional } from './utils'
-
-export { default as LoginCard } from '../..\\components\\loginCard.vue'
-export { default as Logo } from '../..\\components\\Logo.vue'
-export { default as VuetifyLogo } from '../..\\components\\VuetifyLogo.vue'
-
-export const LazyLoginCard = import('../..\\components\\loginCard.vue' /* webpackChunkName: "components/login-card" */).then(c => wrapFunctional(c.default || c))
-export const LazyLogo = import('../..\\components\\Logo.vue' /* webpackChunkName: "components/logo" */).then(c => wrapFunctional(c.default || c))
-export const LazyVuetifyLogo = import('../..\\components\\VuetifyLogo.vue' /* webpackChunkName: "components/vuetify-logo" */).then(c => wrapFunctional(c.default || c))
