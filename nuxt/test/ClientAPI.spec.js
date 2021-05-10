accef6b660cde3e62ec0e5d83a04f0fd6815851b
Samuel Nunoo <38366722+samuelnunoo@users.noreply.github.com>
2021-05-09 20:20:02 -0700

    Rose and I went through and added a test method and we also worked on the removeDocument() method. We used VSCode Liveshae so our commits aren't done from one machine

--- nuxt/test/ClientAPI.spec.js
+++
@@ -1,1 +0,0 @@
-import * as ClientAPI from "../services/ClientAPI"
--- nuxt/test/Editor.spec.js
+++ nuxt/test/Editor.spec.js
@@ -1,12 +1,14 @@
-/*import EditorV from "../pages/components/Editor.vue"
+import EditorV from "../pages/Editor.vue"
 import { mount } from '@vue/test-utils'
+import StarterKit from '@tiptap/starter-kit'
 import { Editor } from '@tiptap/vue-2'
 
 describe('Editor.vue', () => {
     it('can recognize default text', () => {
-      const wrapper = mount(EditorV)
-      expect(wrapper.vm.$data.editor.getHTML()).toBe('<p>Hello</p>')
+      //@todo 
+
+     const editor =  new Editor({content:'<p>Hello </p>',extensions: [StarterKit]})
+
+     expect(editor.getHTML()).toBe("<p>Hello </p>")
     })
   })
-
-  */
